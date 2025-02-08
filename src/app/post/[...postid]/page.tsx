8acd1c5405110e56f56ffe2d19646814f1198615
Satria-WS <extencezone02@gmail.com>
2025-02-08 22:26:44 +0700

    add PostDetail and ProductList components

---
+++ src/app/post/[...postid]/page.tsx
@@ -0,0 +1,12 @@
+const PostDetail = async ({ params }: { params: { postid: string } }) => {
+  console.log("hello world");
+  const awaitedParams = await params;
+  console.log(awaitedParams);
+  return (
+    <>
+      <div className="text-red-500">Postx{awaitedParams.postid}</div>
+      <div>Postx{awaitedParams.postid[0]}</div>
+    </>
+  );
+};
+export default PostDetail;
